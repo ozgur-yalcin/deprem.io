209e2983a0c2e9f40d2d83125bea83a2b80cf21c
Özgür Yalçın <ozguryalcin@outlook.com>
2023-02-10 05:44:00 +0300

    Update cache.go

--- cache/cache.go
+++ cache/cache.go
@@ -7,10 +7,15 @@ import (
 	"log"
 	"time"
 
+	"github.com/ozgur-soft/deprem.io/environment"
 	"github.com/redis/go-redis/v9"
 )
 
-var Client *redis.Client
+var client *redis.Client
+
+func Connect() {
+	client = redis.NewClient(&redis.Options{Network: "tcp", Addr: environment.RedisHost + ":" + environment.RedisPort, Password: environment.RedisPass, DB: 10})
+}
 
 func Key(prefix, key string) string {
 	hash := md5.New()
@@ -21,7 +26,7 @@ func Key(prefix, key string) string {
 }
 
 func Get(ctx context.Context, key string) []byte {
-	data, err := Client.Get(ctx, key).Bytes()
+	data, err := client.Get(ctx, key).Bytes()
 	if err != nil {
 		return nil
 	}
@@ -29,7 +34,7 @@ func Get(ctx context.Context, key string) []byte {
 }
 
 func Exists(ctx context.Context, key string) (exists bool) {
-	data, err := Client.Exists(ctx, key).Result()
+	data, err := client.Exists(ctx, key).Result()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -42,25 +47,25 @@ func Exists(ctx context.Context, key string) (exists bool) {
 }
 
 func Delete(ctx context.Context, key string) {
-	if err := Client.Del(ctx, key).Err(); err != nil {
+	if err := client.Del(ctx, key).Err(); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func Set(ctx context.Context, key string, val interface{}, exp int) {
-	if err := Client.Set(ctx, key, val, time.Duration(exp)*time.Second).Err(); err != nil {
+	if err := client.Set(ctx, key, val, time.Duration(exp)*time.Second).Err(); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func Expire(ctx context.Context, key string, exp int) {
-	if err := Client.Expire(ctx, key, time.Duration(exp)*time.Second).Err(); err != nil {
+	if err := client.Expire(ctx, key, time.Duration(exp)*time.Second).Err(); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func Flush(ctx context.Context) {
-	if err := Client.FlushDB(ctx).Err(); err != nil {
+	if err := client.FlushDB(ctx).Err(); err != nil {
 		log.Fatal(err)
 	}
 }
